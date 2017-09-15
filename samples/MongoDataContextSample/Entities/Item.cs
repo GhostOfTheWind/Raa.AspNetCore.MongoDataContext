89f279f8b0dc6c33d1fe65d35dd0ae4dec0f7dff
Mateusz Szmytko <mateusz.szmytko@gmail.com>
2017-09-15 21:51:02 +0200

    cleaning

---
+++ samples/MongoDataContextSample/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using Raa.AspNetCore.MongoDataContext.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDataContextSample.Entities
+{
+    public class Item : IEntity<ObjectId>
+    {
+
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
