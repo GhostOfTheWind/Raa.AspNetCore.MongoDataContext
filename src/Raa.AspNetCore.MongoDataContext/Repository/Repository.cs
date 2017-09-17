f92be6a54f799055a9cc32f085e2dc912c6d515a
Mateusz Szmytko <mateusz.szmytko@gmail.com>
2017-09-17 13:11:38 +0200

    refactor repository

--- src/Raa.AspNetCore.MongoDataContext/Repository/Repository.cs
+++ src/Raa.AspNetCore.MongoDataContext/Repository/Repository.cs
@@ -9,6 +9,131 @@ using System.Threading.Tasks;
 
 namespace Raa.AspNetCore.MongoDataContext.Repository
 {
+    public class RepositoryBase<TEntity, TContext, TEntityKey>
+        where TContext : MongoDataContext
+        where TEntity : IEntity<TEntityKey>
+    {
+        private string _collectionName;
+        public string CollectionName => _collectionName;
+
+        private TContext _context;
+
+        private IMongoCollection<TEntity> _collection;
+        public IMongoCollection<TEntity> Collection => _collection;
+
+        public IQueryable<TEntity> List => _collection.AsQueryable();
+
+        public RepositoryBase(TContext context) : this(context, null)
+        {
+
+        }
+
+        public RepositoryBase(TContext context, string collectionName)
+        {
+            _collectionName = collectionName != null ? collectionName : typeof(TEntity).Name + "s";
+            _context = context;
+
+            _collection = _context.Database.GetCollection<TEntity>(_collectionName);
+        }
+    }
+
+    public class Repository<TEntity, TContext, TEntityKey> : RepositoryBase<TEntity, TContext, TEntityKey>
+        where TContext : MongoDataContext
+        where TEntity : IEntity<TEntityKey>
+
+    {
+
+        public Repository(TContext context) : this(context, null)
+        {
+            
+        }
+
+        public Repository(TContext context, string collectionName) : base(context, collectionName)
+        {
+        }
+
+        public virtual async Task<TEntity> InsertAsync(TEntity e)
+        {
+            if (e == null) { throw new ArgumentNullException(nameof(e)); }
+
+
+            await Collection.InsertOneAsync(e);
+
+            return e;
+        }
+
+        public virtual Task<TEntity> FindByIdAsync(TEntityKey id)
+        {
+            return Collection.Find(u => u.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public virtual async Task UpdateAsync(TEntity e)
+        {
+            await Collection.ReplaceOneAsync<TEntity>(i => i.Id.Equals(e.Id), e);
+        }
+
+        public virtual async Task DeleteAsync(TEntity e)
+        {
+            await Collection.DeleteOneAsync<TEntity>(i => i.Id.Equals(e.Id));
+        }
+
+        public virtual async Task DeleteAsync(TEntityKey eId)
+        {
+            await Collection.DeleteOneAsync<TEntity>(i => i.Id.Equals(eId));
+        }
+
+        public virtual Task<TEntity> FindOneAsync(Expression<Func<TEntity, bool>> filterFunc)
+        {
+            return Collection.Find(filterFunc).FirstOrDefaultAsync();
+        }
+        public virtual Task<List<TEntity>> FindManyAsync(Expression<Func<TEntity, bool>> filterFunc)
+        {
+            return Collection.Find(filterFunc).ToListAsync();
+        }
+
+        public virtual Task<long> CountAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return Collection.CountAsync(filter);
+        }
+    }
+
+    
+    public class Repository<TEntity, TContext> : Repository<TEntity, TContext, ObjectId>
+        where TContext : MongoDataContext
+        where TEntity : IEntity<ObjectId>
+    {
+        
+        public Repository(TContext context, string collectionName) : base(context, collectionName)
+        {
+            
+        }
+
+        public Repository(TContext context) : this(context, null)
+        {
+        }
+
+        public override async Task<TEntity> InsertAsync(TEntity e) 
+        {
+            if (e == null) { throw new ArgumentNullException(nameof(e)); }
+
+            e.Id = ObjectId.GenerateNewId();
+            
+            await Collection.InsertOneAsync(e);
+
+            return e;
+        }
+
+
+        public Task<TEntity> FindByIdAsync(string id)
+        {
+            ObjectId parsedId;
+            ObjectId.TryParse(id, out parsedId);
+
+            return FindByIdAsync(parsedId);
+        }
+
+    }
+
     public class Repository<TEntity> : Repository<TEntity, MongoDataContext>
         where TEntity : IEntity<ObjectId>
     {
@@ -20,99 +145,5 @@ namespace Raa.AspNetCore.MongoDataContext.Repository
         public Repository(MongoDataContext context, string collectionName) : base(context, collectionName)
         {
         }
-    }
-    public class Repository<TEntity, TContext>
-        where TContext : MongoDataContext
-        where TEntity : IEntity<ObjectId>
-    {
-        private string _collectionName;
-        public string CollectionName => _collectionName; 
-
-
-        private TContext _context;
-
-        private IMongoCollection<TEntity> _collection;
-        public virtual IQueryable<TEntity> List => _collection.AsQueryable();
-
-        public Repository(TContext context, string collectionName)
-        {
-            _collectionName = collectionName != null? collectionName : typeof(TEntity).Name + "s";
-            _context = context;
-
-            _collection = _context.Database.GetCollection<TEntity>(_collectionName);
-        }
-
-        public Repository(TContext context) : this(context, null)
-        {
-        }
-
-        public async Task<TEntity> InsertAsync(TEntity e)
-        {
-            if (e == null) { throw new ArgumentNullException(nameof(e)); }
-
-            e.Id = ObjectId.GenerateNewId();
-            
-            await _collection.InsertOneAsync(e);
-
-            return e;
-        }
-
-        public async Task UpdateAsync(TEntity e)
-        {
-            await _collection.ReplaceOneAsync<TEntity>(i => i.Id == e.Id, e);
-        }
-
-
-        public async Task DeleteAsync(TEntity e)
-        {
-            await _collection.DeleteOneAsync<TEntity>(i => i.Id == e.Id);
-        }
-
-        public async Task DeleteAsync(ObjectId eId)
-        {
-            await _collection.DeleteOneAsync<TEntity>(i => i.Id == eId);
-        }
-
-        public async Task DeleteAsync(IEnumerable<TEntity> eList)
-        {
-            foreach(TEntity e in eList)
-            {
-                await _collection.DeleteOneAsync<TEntity>(i => i.Id == e.Id);
-            }
-        }
-
-        public Task<TEntity> FindByIdAsync(ObjectId id)
-        {
-
-            return _collection.Find(u => u.Id == id).FirstOrDefaultAsync();
-        }
-
-        public Task<TEntity> FindByIdAsync(string id)
-        {
-            ObjectId parsedId;
-            ObjectId.TryParse(id, out parsedId);
-
-            return FindByIdAsync(parsedId);
-        }
-            
-        
-
-        public Task<TEntity> FindOneAsync(Expression<Func<TEntity, bool>> filterFunc)
-        {
-            return _collection.Find(filterFunc).FirstOrDefaultAsync();
-        }
-        public Task<List<TEntity>> FindAsync(Expression<Func<TEntity, bool>> filterFunc)
-        {
-            return _collection.Find(filterFunc).ToListAsync();
-        }
-
-        public Task<long> CountAsync(Expression<Func<TEntity, bool>> filter = null)
-        {
-            return _collection.CountAsync(filter);
-        }
-
-
-
-
     }
 }
